59607b2253b732ad28a113fa8c86b7f669b7b168
Viacheslav Zhivetev <slavyan6363@gmail.com>
2021-01-18 16:38:33 +0300

    added two buttons

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,22 @@ USBHID HID;
 HIDJoystick Joystick(HID);
 
 #define POTENTIOMETER_PIN PA0
+#define BUTTON1_PIN PB0
+#define BUTTON2_PIN PB1
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-
   HID.begin(HID_JOYSTICK);
   while (!USBComposite);
 
   pinMode(POTENTIOMETER_PIN, INPUT_ANALOG);
+  pinMode(BUTTON1_PIN, INPUT_PULLDOWN);
+  pinMode(BUTTON2_PIN, INPUT_PULLDOWN);
 }
 
 void loop() {
   float val = analogRead(POTENTIOMETER_PIN) / 4 / 0.7f; // 0–4095
   Joystick.X((uint16)val);
+
+  Joystick.button(1, digitalRead(BUTTON1_PIN) == HIGH);
+  Joystick.button(2, digitalRead(BUTTON2_PIN) == HIGH);
 }
