4ab35f785fce1ce5c74d272e3af62949220699ae
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2021-09-09 01:50:00 +0300

    log loadcell values to serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <USBComposite.h>
+#include <HX711.h>
 
 USBHID HID;
 HIDJoystick Joystick(HID);
@@ -8,19 +9,80 @@ HIDJoystick Joystick(HID);
 #define BUTTON1_PIN PB0
 #define BUTTON2_PIN PB1
 
-void setup() {
-  HID.begin(HID_JOYSTICK);
-  while (!USBComposite);
 
-  pinMode(POTENTIOMETER_PIN, INPUT_ANALOG);
-  pinMode(BUTTON1_PIN, INPUT_PULLDOWN);
-  pinMode(BUTTON2_PIN, INPUT_PULLDOWN);
+// HX711 circuit wiring
+#define LOADCELL_DOUT_PIN PB9
+#define LOADCELL_SCK_PIN PB8
+
+int8_t led = 0;
+
+HX711 scale;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+
+  for (int i = 0; i < 15; i++) {
+    Serial.print("waiting... ");
+    Serial.println(i);
+    delay(100);
+    led = ~ led & 1;
+    digitalWrite(LED_BUILTIN, led);
+  }
+
+  Serial.println("HX711 Demo");
+  Serial.println("Initializing the scale");
+
+  // Initialize library with data output pin, clock input pin and gain factor.
+  // Channel selection is made by passing the appropriate gain:
+  // - With a gain factor of 64 or 128, channel A is selected
+  // - With a gain factor of 32, channel B is selected
+  // By omitting the gain factor parameter, the library
+  // default "128" (Channel A) is used here.
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());			// print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
+						// by the SCALE parameter (not set yet)
+
+  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();				        // reset the scale to 0
+
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+						// by the SCALE parameter set with set_scale
+
+  Serial.println("Readings:");
 }
 
 void loop() {
-  float val = analogRead(POTENTIOMETER_PIN) / 4 / 0.7f; // 0–4095
-  Joystick.X((uint16)val);
+  Serial.print("one reading:\t");
+  Serial.print(scale.get_units(), 1);
+  Serial.print("\t| average:\t");
+  Serial.println(scale.get_units(10), 1);
 
-  Joystick.button(1, digitalRead(BUTTON1_PIN) == HIGH);
-  Joystick.button(2, digitalRead(BUTTON2_PIN) == HIGH);
+  // scale.power_down();			        // put the ADC in sleep mode
+  delay(10);
+  // scale.power_up();
 }
